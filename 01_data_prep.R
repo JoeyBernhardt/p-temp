a73103ae6120d4747016ff6783f94cee4dabd729
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-04-07 09:22:57 -0700

    adding april 5 run summaries

--- 01_data_prep.R
+++ 01_data_prep.R
@@ -4,11 +4,17 @@ library(dplyr)
 library(purrr)
 library(ggplot2)
 
+
+#### Step 1 #### 
+
+## in the shell, use this command to copy the summary files from the folder of flow cam run folders to a summary-only file
 ## cp **/*summary.csv /Users/Joey/Desktop/run-summaries
 
-fnams <- list.files("/Users/Joey/Documents/p-temp/run-summaries", full.names = TRUE) ## find out the names of all the files in data-summary, use full.names to get the relative path for each file
+#### Step 2: create a list of file names for each of the summaries ####
+fnams <- list.files("/Users/Joey/Documents/p-temp/run-summaries-March29", full.names = TRUE) ## find out the names of all the files in data-summary, use full.names to get the relative path for each file
 
-ptemp_summaries <- fnams %>%  
+#### Step 3: create a df with the dataset ID and the cell count ####
+ptemp_summaries_march29 <- fnams %>%  
 	lapply(FUN = function(p) read.csv(p)) %>% 
 	as.data.frame(.) %>% 
 	filter(List.File == "Particles / ml") %>% 
@@ -21,13 +27,15 @@ ptemp_summaries <- fnams %>%
 
 View(ptemp_summaries)
 
-write_csv(ptemp_summaries, "ptemp_summaries.csv")
+ptemp_sep_29 <- separate(ptemp_summaries_march29, dataset, c("treatment", "temperature", "replicate", "date"), extra = "drop")
 
-ptemp_sep <- separate(ptemp_summaries, dataset, c("treatment", "temperature", "replicate", "date"))
 
-View(ptemp_sep)
+#### Step 4: write out the df to a csv ####
+write_csv(ptemp_sep_29, "ptemp_summaries_March29.csv")
 
-ggplot(ptemp_sep, aes(y = cell_count, x = temperature, color = treatment)) + geom_point()
+
+
+ggplot(ptemp_sep_29, aes(y = cell_count, x = temperature, color = treatment)) + geom_point()
 
 ### March 30
 
@@ -64,7 +72,7 @@ all <- rbind(march29, march30)
 all <- unite(all, uniqueID, treatment, temperature, replicate, sep = "_", remove = FALSE)
 
 all$date[all$date == "MARCH24"] <- "MARCH29" 
-?unite
+
 
 
 ggplot(all, aes(x = date, y = cell_count, group = uniqueID, color = factor(all$treatment))) + geom_line() +ylim(0,20000)
@@ -88,4 +96,52 @@ ptemp_summaries_April2 <- fnams %>%
 ptemp_sep_April2 <- separate(ptemp_summaries_April2, dataset, c("UniqueID", "treatment", "temperature", "replicate", "date", "try", "type"))
 
 ggplot(ptemp_sep_April2, aes(x = temperature, y = cell_count, group = treatment, color = factor(treatment))) + geom_point()
+
+
+#### April 5 ####
+
+
+fnams <- list.files("/Users/Joey/Documents/p-temp/run-summaries-April5", full.names = TRUE) ## find out the names of all the files in data-summary, use full.names to get the relative path for each file
+
+#### Step 3: create a df with the dataset ID and the cell count ####
+ptemp_summaries_april5 <- fnams %>%  
+	lapply(FUN = function(p) read.csv(p)) %>% 
+	as.data.frame(.) %>% 
+	filter(List.File == "Particles / ml") %>% 
+	select(- starts_with("List")) %>%
+	t(.) %>%
+	as.data.frame() %>%
+	mutate(dataset = rownames(.)) %>%
+	mutate(cell_count = as.numeric(as.character(V1))) %>%
+	select(-V1)
+
+View(ptemp_summaries_april5)
+
+ptemp_sep_april5 <- separate(ptemp_summaries_april5, dataset, c("UniqueID", "date", "try"), extra = "drop")
+
+?separate
+
+sep_april5 <- separate(ptemp_sep_april5, UniqueID, c("x", "Unique_ID"), sep = 1) %>% 
+	select(-1)
+
+Unique_ID_key <- read_csv("P-TEMP-UniqueID-key.csv")
+
+Unique_ID_key <- separate(Unique_ID_key, TREATMENT_ID, c("treatment", "temperature", "replicate"))
+sep_april5 <- sep_april5 %>% 
+	rename(UniqueID = Unique_ID)
+sep_april5$UniqueID <- as.integer(sep_april5$UniqueID)
+	
+april_5_cellcount <- left_join(sep_april5, Unique_ID_key, by = "UniqueID")
+
+ggplot(april_5_cellcount, aes(x = temperature, y = cell_count, group = treatment, color = factor(treatment))) + geom_point()
+
+april_5_cellcount %>% 
+	filter(replicate %in% c("1", "2", "3", "4", "5", "6")) %>% 
+	ggplot(., aes(x = as.factor(temperature), y = cell_count, fill = factor(treatment), geom = "boxplot")) +
+	geom_boxplot()
+	
+	
+	
+	
+
 
