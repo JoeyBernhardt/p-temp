2a7c328ba6e725b506999be49c99faaf918aab71
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-11-24 10:18:20 -0800

    adding schoolfield Eas to the p-temp results megafile :tada:

--- Rscripts/08_growth_rates_phytoplankton.R
+++ Rscripts/08_growth_rates_phytoplankton.R
@@ -26,7 +26,6 @@ str(ptemp)
 ptemp <- ptemp %>% 
 	mutate(sample_date = mdy(sample_date))
 
-
 ptemp$start.time <- ymd_hms("2016-03-30 14:15:43")
 ptemp$time_since_innoc <- interval(ptemp$start.time, ptemp$sample_date)
 
@@ -35,15 +34,29 @@ ptemp2 <- ptemp %>%
 	mutate(time_since_innoc_days = time_since_innoc/ddays(1)) %>% 
 	mutate(time_since_innoc_hours = time_since_innoc/dhours(1))
 
-ptemp$start.time <- ymd_hms("2016-03-30 14:15:43")
-ptemp$time_since_innoc <- interval(ptemp$start.time, ptemp$sample_date)
+### now with ptemp_algae
+
+ptemp_algae$month_day <- NA
+ptemp_algae$month_day[ptemp_algae$date == "MARCH29"] <- "2016-03-29"
+ptemp_algae$month_day[ptemp_algae$date == "APRIL1"] <- "2016-04-01"
+ptemp_algae$month_day[ptemp_algae$date == "APRIL5"] <- "2016-04-05"
+ptemp_algae$month_day[ptemp_algae$date == "APRIL8"] <- "2016-04-08"
+ptemp_algae$month_day[ptemp_algae$date == "APRIL12"] <- "2016-04-12"
+ptemp_algae$month_day[ptemp_algae$date == "APRIL15"] <- "2016-04-15"
+ptemp_algae$month_day[ptemp_algae$date == "APRIL19"] <- "2016-04-19"
+ptemp_algae$month_day[ptemp_algae$date == "APRIL26"] <- "2016-04-26"
+ptemp_algae$month_day[ptemp_algae$date == "MAY4"] <- "2016-05-04"
+
+ptemp_algae <- ptemp_algae %>% 
+	mutate(sample_date = ymd(month_day))
+
+ptemp_algae$start.time <- ymd_hms("2016-03-28 14:15:43")
+ptemp_algae$time_since_innoc <- interval(ptemp_algae$start.time, ptemp_algae$sample_date)
 
 
-ptemp2 <- ptemp %>% 
+ptemp_algae2 <- ptemp_algae %>% 
 	mutate(time_since_innoc_days = time_since_innoc/ddays(1)) %>% 
 	mutate(time_since_innoc_hours = time_since_innoc/dhours(1))
-
-
 
 
 
@@ -97,26 +110,6 @@ mod <- lm(log(max) ~ temp*P, data =ptemp_algae_control_summary)
 summary(mod)
 
 
-
-ptemp_algae$month_day <- NA
-ptemp_algae$month_day[ptemp_algae$date == "MARCH29"] <- "2016-03-29"
-ptemp_algae$month_day[ptemp_algae$date == "APRIL1"] <- "2016-04-01"
-ptemp_algae$month_day[ptemp_algae$date == "APRIL5"] <- "2016-04-05"
-ptemp_algae$month_day[ptemp_algae$date == "APRIL8"] <- "2016-04-08"
-ptemp_algae$month_day[ptemp_algae$date == "APRIL12"] <- "2016-04-12"
-ptemp_algae$month_day[ptemp_algae$date == "APRIL15"] <- "2016-04-15"
-ptemp_algae$month_day[ptemp_algae$date == "APRIL19"] <- "2016-04-19"
-ptemp_algae$month_day[ptemp_algae$date == "APRIL26"] <- "2016-04-26"
-ptemp_algae$month_day[ptemp_algae$date == "MAY4"] <- "2016-05-04"
-
-ptemp_algae <- ptemp_algae %>% 
-	mutate(month_day = ymd(month_day))
-
-sum(is.na(ptemp_algae$month_day))
-sum(is.na(ptemp_algae$date))
-
-unique(ptemp_algae$date)
-
 ptemp_algae %>% 
 	filter(!grepl("C", replicate)) %>% 
 	ggplot(data = ., aes(x = month_day, y = biovol, group = ID, color = factor(temp))) + geom_line(aes(linetype = P), size = 2) +
@@ -150,10 +143,6 @@ wdaph_final_day <- ptemp_algae %>%
 	scale_y_log10()
 
 
-
-
-
-
 # logistic regressions ----------------------------------------------------
 
 sum(is.na(ptemp2$algal_biovolume))
@@ -164,9 +153,9 @@ ptemp2 <- ptemp2 %>%
 
 ## get starting biovolumes
 
-ptemp2 %>% 
-	filter(date == "01-Apr") %>% 
-	summarise(mean_biovol = mean(algal_biovolume)) %>% View
+ptemp_algae2 %>% 
+	filter(sample_date == "2016-03-29") %>% 
+	summarise(mean_biovol = mean(biovol)) %>% View
 
 ## take out unique ID 37, 26, 27, 29, 45, 31, 33, 34, 35, 36
 
@@ -178,16 +167,16 @@ r20 <- ptemp2 %>%
 							data= .,  start=list(a=0.01),
 							control = nls.control(maxiter=100, minFactor=1/204800000))))
 
-ptemp2 %>%
+ptemp_algae2 %>%
 	# filter(phosphorus_treatment == "FULL") %>%
 	# filter(!grepl('37|26|27|29|45|31|33|34|35|36', unique_ID)) %>% 
-	group_by(unique_ID, phosphorus_treatment, temperature) %>%
-	do(tidy(nls(algal_biovolume ~ 13830995 * (1+a)^(time_since_innoc_days),
+	group_by(ID, P, temp) %>%
+	do(tidy(nls(biovol ~ 21435402 * (1+a)^(time_since_innoc_days),
 							data= .,  start=list(a=0.01),
 							control = nls.control(maxiter=1000, minFactor=1/204800000)))) %>% 
-	group_by(phosphorus_treatment, temperature) %>% 
+	group_by(P, temp) %>% 
 	summarise_each(funs(mean, std.error), estimate) %>% 
-	ggplot(data =., aes(x = temperature, y = mean, color = phosphorus_treatment)) + geom_point() +
+	ggplot(data =., aes(x = temp, y = mean, color = P)) + geom_point() +
 	geom_errorbar(aes(ymin = mean-std.error, ymax = mean + std.error))
 
 growth_rates_r <- ptemp2 %>%
@@ -195,7 +184,7 @@ growth_rates_r <- ptemp2 %>%
 	do(tidy(nls(algal_biovolume ~ 13830995 * (1+a)^(time_since_innoc_days),
 							data= .,  start=list(a=0.01),
 							control = nls.control(maxiter=1000, minFactor=1/204800000)))) %>% 
-	filter(temperature != "24") %>% 
+	# filter(temperature != "24") %>% 
 	mutate(inverse_temp = (-1/(.00008617*(temperature+273.15)))) %>%
 	group_by(phosphorus_treatment) %>% 
 	do(tidy(lm(log(estimate) ~ inverse_temp, data = .), conf.int = TRUE)) %>% View
--- Rscripts/p-temp-results.md
+++ Rscripts/p-temp-results.md
@@ -70,4 +70,133 @@ algae_summaries %>%
 
 ![](p-temp-results_files/figure-html/unnamed-chunk-5-1.png)<!-- -->
 
+Activation energies of phytoplankton densitiies, a la Schoolfield
+
+
+```r
+current_dataset <- algae_summaries %>% 
+	filter(P == "FULL", consumer == "absent") %>%
+	select(max, temp) %>% 
+	mutate(K = temp + 273.15) %>% 
+	rename(OriginalTraitValue = max) %>% 
+	select(-temp)
+
+current_dataset_def <- algae_summaries %>% 
+	filter(P == "DEF", consumer == "absent") %>%
+	select(max, temp) %>% 
+	mutate(K = temp + 273.15) %>% 
+	rename(OriginalTraitValue = max) %>% 
+	select(-temp)
+```
+
+
+term      estimate      std.error    statistic     p.value  phosphorus 
+-----  -----------  -------------  -----------  ----------  -----------
+B0       18.700827   2.250786e-01   83.0857648   0.0000000  replete    
+E         2.329546   6.108948e-01    3.8133334   0.0041319  replete    
+E_D      31.523532   1.012739e+07    0.0000031   0.9999976  replete    
+T_h     296.354656   1.715220e+05    0.0017278   0.9986591  replete    
+B0       18.858266   4.731711e-01   39.8550650   0.0000000  deficient  
+E         1.233073   1.284253e+00    0.9601481   0.3620638  deficient  
+E_D      30.348379   2.945930e+07    0.0000010   0.9999992  deficient  
+T_h     296.420100   4.542636e+05    0.0006525   0.9994936  deficient  
+
+Plot the activation energies, daphnia absent
+
+
+```r
+all_estimates %>% 	
+filter(term == "E") %>%
+ggplot(aes(x = phosphorus, y = estimate, group = phosphorus)) + geom_point() +
+	geom_errorbar(aes(ymin = estimate - std.error, ymax = estimate + std.error))
+```
+
+![](p-temp-results_files/figure-html/unnamed-chunk-8-1.png)<!-- -->
+
+Generate predictions from the model fit (non bootstrapped) ---------------------------------
+
+```r
+ tmp_temps <- seq(min(
+	floor(current_dataset$K)), 
+	ceiling(max(current_dataset$K)
+	), length = 200)
+
+tmp_model <- exp(Schoolfield(
+	coef(schoolfield_nls_full)["B0"],
+	coef(schoolfield_nls_full)["E"],
+	coef(schoolfield_nls_full)["E_D"],
+	coef(schoolfield_nls_full)["T_h"],
+	tmp_temps
+))
+
+
+ModelToPlotS <- data.frame(
+	Temperature = tmp_temps - 273.15, 
+	TraitValue = tmp_model
+)
+
+DataToPlot <- data.frame(
+	Temperature = current_dataset$K - 273.15, 
+	TraitValue = current_dataset$OriginalTraitValue
+)
+DataToPlot <- na.omit(DataToPlot)
+
+
+
+##### DEF 
+
+tmp_model_def <- exp(Schoolfield(
+	coef(schoolfield_nls_def)["B0"],
+	coef(schoolfield_nls_def)["E"],
+	coef(schoolfield_nls_def)["E_D"],
+	coef(schoolfield_nls_def)["T_h"],
+	tmp_temps
+))
+
+
+ModelToPlotS_def <- data.frame(
+	Temperature = tmp_temps - 273.15, 
+	TraitValue = tmp_model_def
+)
+
+DataToPlot_def <- data.frame(
+	Temperature = current_dataset_def$K - 273.15, 
+	TraitValue = current_dataset_def$OriginalTraitValue
+)
+DataToPlot_def <- na.omit(DataToPlot_def)
+```
+
+
+plot them!
+
+```r
+full_plot <- ggplot() + geom_point(data = DataToPlot, aes(x = Temperature, 
+																						 y = TraitValue), size = 3, col = "black", bg = "lightcyan2", 
+											alpha = 0.7, pch = 21) + 
+	geom_line(data = ModelToPlotS, 
+						aes(x = Temperature, y = TraitValue), colour = "#1b9e77", 
+						lwd = 1.3) +                           
+	ggtitle("Phosphorus replete") +
+	xlab(expression(paste("Temperature (", degree, C, ")"))) + 
+	ylab("max phytoplankton abundance") +
+	theme_bw() + theme(plot.title = element_text(size = 16), 
+										 axis.title = element_text(size = 16))
+
+#### DEF
+def_plot <- ggplot() + geom_point(data = DataToPlot_def, aes(x = Temperature, 
+																						 y = TraitValue), size = 3, col = "black", bg = "lightcyan2", 
+											alpha = 0.7, pch = 21) + 
+	geom_line(data = ModelToPlotS_def, 
+						aes(x = Temperature, y = TraitValue), colour = "#1b9e77", 
+						lwd = 1.3) +                           
+	ggtitle("Phosphorus deficient") +
+	xlab(expression(paste("Temperature (", degree, C, ")"))) + 
+	ylab("max phytoplankton abundance") +
+	theme_bw() + theme(plot.title = element_text(size = 16), 
+										 axis.title = element_text(size = 16)) + scale_y_log10()
+
+grid.arrange(full_plot, def_plot, ncol = 2)
+```
+
+![](p-temp-results_files/figure-html/unnamed-chunk-10-1.png)<!-- -->
 
