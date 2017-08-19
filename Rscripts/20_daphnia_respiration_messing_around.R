cfa785449f1e7bf8dfecf1dc4dcf775d5203ae67
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2017-08-18 17:36:37 -0700

    changes to mr plot

--- Rscripts/20_daphnia_respiration_messing_around.R
+++ Rscripts/20_daphnia_respiration_messing_around.R
@@ -22,17 +22,18 @@ mr %>%
 plot_data %>% 
 	mutate(mass = exp(log_mass_mg)) %>% 
 	mutate(respiration_rate = exp(log_respiration_mg_hour)) %>% 
-	ggplot(aes(x = log(mass), y = log(respiration_rate))) + geom_point(size = 2) +
+	ggplot(aes(x = mass, y = respiration_rate)) + geom_point(size = 2) +
 	geom_smooth(method = "lm", color = "black") +
 	theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
 				panel.background = element_blank(),
-				axis.line = element_line(color="black")) +
-	theme(text = element_text(size=16, family = "Helvetica")) + ylab('Log metabolic rate (mg oxygen/L/hour)') + xlab("Log mass (mg DW)") + 
-	annotate("text", x = -4.49, y= -2.5, label = "b = 1.44, CI (1.01, 1.89)", size = 6)
-ggsave("p-temp-figures_files/daphnia_metabolic_rate_per_mass.pdf")
-ggsave("p-temp-figures_files/daphnia_metabolic_rate_per_mass.png")
+				axis.line = element_line(color="black"), 
+				panel.border = element_rect(colour = "black", fill=NA, size=1))+
+	theme(text = element_text(size=16, family = "Helvetica"))  + ylab('Log metabolic rate\n (mg oxygen/L/hour)') + xlab("Log mass (mg DW)") + 
+	annotate("text", x = 0.0115, y= 0.08, label = "b = 1.44, CI (1.01, 1.89)", size = 6) + scale_y_log10(breaks = scales::pretty_breaks(n = 3))+ scale_x_log10(breaks = scales::pretty_breaks(n = 3))
+ggsave("p-temp-figures_files/daphnia_metabolic_rate_per_mass.pdf", width = 5, height = 4)
+ggsave("p-temp-figures_files/daphnia_metabolic_rate_per_mass.png", width = 5, height = 4)
 
-
+?scale_y_continuous()
 
 plot_data %>% 
 	mutate(mass = exp(log_mass_mg)) %>% 
